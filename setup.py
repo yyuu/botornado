b49d53457c331a4ddf12c2167bf6cb2abbae529f
Yamashita Yuu <yamashita@geishatokyo.com>
2012-02-15 16:22:06 +0900

    release/0.0.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='botornado',
-    version='0.0.2git',
+    version='0.0.2',
     description='boto on tornado - an asynchronous Amazon Web Service (AWS) client',
     author='Yamashita, Yuu',
     author_email='yamashita@geishatokyo.com',
