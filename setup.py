5c1d47eee9f1e364c4eb136048fe6020d2285a8a
Yamashita Yuu <yamashita@geishatokyo.com>
2012-03-21 15:16:34 +0900

    version bumped to 0.0.4git

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='botornado',
-    version='0.0.3',
+    version='0.0.4git',
     description='boto on tornado - an asynchronous Amazon Web Service (AWS) client',
     author='Yamashita, Yuu',
     author_email='yamashita@geishatokyo.com',
